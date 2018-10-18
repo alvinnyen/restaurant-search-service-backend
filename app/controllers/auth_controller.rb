05d8bf704f9bb2c154af4a02633cfa6cd9c91f00
alvinnyen <alvinyen219@gmail.com>
2018-10-18 22:15:18 +0800

    add jwt to auth#login login

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,7 +4,12 @@ class AuthController < ApplicationController
         user = User.find_by(username: params[:username])
         if user && user.authenticate(params[:password])
             # puts user.authenticate(params[:password])
-            render json: user
+
+            payload = { user_id: user.id }
+            token = JWT.encode(payload, 'secret')
+
+            # render json: user
+            render json: {user: user, jwt: token}
         end
     end
 end
